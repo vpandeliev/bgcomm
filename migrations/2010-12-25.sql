41391e8dcbb7c85ef2aee86341155383ec5078b2
velian <vpandeliev@gmail.com>
2010-12-25 19:51:11 -0500

    new christmas migration

--- migrations/2010-12-25.sql
+++ migrations/2010-12-25.sql
@@ -1,5 +1,4 @@
 BEGIN;
-ALTER TABLE "posts_page" ADD COLUMN "hide" boolean NOT NULL DEFAULT 0;
 ALTER TABLE "posts_post" ADD COLUMN "hide" boolean NOT NULL DEFAULT 0;
 ALTER TABLE "posts_event" ADD COLUMN "hide" boolean NOT NULL DEFAULT 0;
 ALTER TABLE "posts_member" ADD COLUMN "paid" boolean NOT NULL DEFAULT 0;
